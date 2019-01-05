7a685e07beab4d070cf7adec01e82d384e4c9591
susmitha1mohan <42869445+susmitha1mohan@users.noreply.github.com>
2019-01-05 13:45:18 +0530

    front end and back end integration

---
+++ ecFrontend/src/main/java/controller/ProductController.java
@@ -0,0 +1,45 @@
+package controller;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import model.dao.IProductDAO;
+import model.daoimpl.ProductDAOImpl;
+import model.entity.Product;
+
+@Controller
+public class ProductController {
+	@RequestMapping(value="/addprodpage",method=RequestMethod.GET)
+	public String addProductPage()
+	{
+		return "addproducts";
+	}
+	
+	@RequestMapping(value="/addproducts",method=RequestMethod.POST)
+	public String addProducts(@Valid @ModelAttribute("product1")Product p, BindingResult result)
+	{
+		boolean b = false;
+		if(result.hasErrors())
+		{
+			return "addproducts";
+		}
+		else
+		{
+			IProductDAO pd = new ProductDAOImpl();
+			b = pd.insertProduct(p);
+		}
+		if (b)
+		{
+			return "success";
+		}
+		else
+		{
+			return "addproducts";
+		}
+	}
+
+}
---
+++ ecFrontend/src/main/webapp/views/addproducts.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form:errors path="product1.*"/>
+<form action="${pageContext.request.contextPath}/addproducts" method="POST">
+
+Product Name: <input type="text" name="pName"/>
+
+Product Cost: <input type="text" name="pCost"/>
+
+Product Qty: <input type="text" name="pQty"/>
+
+Product DOM: <input type="text" name="pDOM"/>
+
+<input type="submit" value="Submit"/>
+
+</form>
+</body>
+</html>
